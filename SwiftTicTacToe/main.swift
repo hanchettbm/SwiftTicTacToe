9bc5403509aaf0104b8a25389a2d4333d479a9e3
hanchettbm <hanchettbm@gmail.com>
2021-06-16 13:06:30 -0600

    Add ReadMe

--- SwiftTicTacToe/main.swift
+++ SwiftTicTacToe/main.swift
@@ -9,3 +9,20 @@ import Foundation
 
 print("Hello, World usuing Swift! ")
 
+var inputArray: [String] = []
+print("Enter a word:")
+while let input = readLine() {
+    guard input != "quit" else {
+        break
+    }
+    
+    if !inputArray.contains(input) {
+        inputArray.append(input)
+        print("You entered: \(input)")
+    } else {
+        print("Negative. \"\(input)\" already exits")
+    }
+    
+    print()
+    print("Enter a word:")
+}
